390ba549dc880a6125d673d8f4f16fff745e0fa4
Niklas der Graf <grafniklas98@gmail.com>
2017-07-12 15:20:15 +0200

    Initial Commit for ReportBot

---
+++ Projekt/Reportbot/src/main/java/at/htlgkr/aems/main/Main.java
@@ -0,0 +1,15 @@
+package at.htlgkr.aems.main;
+
+public class Main {
+
+	/**
+	 * Main method. It will be used to call this java program once a day. The program will proceed
+	 * to gather required data from all registered users using the web interface.
+	 * Data will be read from CSS files and stored into the database.
+	 * @param args Additional startup arguments
+	 */
+	public static void main(String[] args) {
+		//TODO: Apply logic here
+	}
+
+}
