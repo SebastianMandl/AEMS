c327e4c1d37859fb9d2ffe9dbf822287f7791601
Niklas der Graf <grafniklas98@gmail.com>
2017-07-14 16:25:44 +0200

    Improved FileDownloader, it is now able to iterate through the first set of meters and download the excel files

--- Projekt/Reportbot/src/main/java/at/htlgkr/aems/filedownload/FileDownloader.java
+++ Projekt/Reportbot/src/main/java/at/htlgkr/aems/filedownload/FileDownloader.java
@@ -1,17 +1,29 @@
 package at.htlgkr.aems.filedownload;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import org.w3c.css.sac.CSSException;
+import org.w3c.css.sac.CSSParseException;
+import org.w3c.css.sac.ErrorHandler;
+
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.IncorrectnessListener;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.ScriptException;
+import com.gargoylesoftware.htmlunit.UnexpectedPage;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.html.HTMLParserListener;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.util.UrlUtils;
+import com.gargoylesoftware.htmlunit.javascript.JavaScriptErrorListener;
 
 import at.htlgkr.aems.database.AemsUser;
 import at.htlgkr.aems.exception.LoginFailedException;
@@ -41,12 +53,28 @@ public class FileDownloader implements Runnable {
     try {
       HtmlPage loginPage = client.getPage(BASE_URL + LOGIN_URL);
       HtmlPage meterPage = login(loginPage);
-      openAllDetails(meterPage);
-      //TODO: Make dynamic | Wait for buttons to load
-      Thread.sleep(3000);
       
+      int detailsCount = openAllDetails(meterPage);
+      
+      /**
+       * HUGE TODO: 
+       * Sometimes the server has an error, a "Sorry, this shouldn't have happened" page shows up.
+       * This does not seem to be predictable, so there will be checks in order to detect and manage
+       * occurances of such events.
+       */
+      
+      int buttonCount = 0;
       do {
-        // TODO: Download excel file and process data, then send to database
+        while(buttonCount < detailsCount) {
+          List<HtmlElement> buttons = waitForDetailsToLoad(meterPage, detailsCount);
+          HtmlElement currentBtn = buttons.get(buttonCount);
+          HtmlPage detailPage = currentBtn.click();
+          InputStream excelInputStream = downloadDetailExcelFile(detailPage);
+          // TODO: Save inputStream into local file
+          clickBackButton(detailPage);
+          buttonCount++;
+        }
+        // click next and repeat process
       } 
       while(moreMetersExist(meterPage));
       
@@ -68,6 +96,7 @@ public class FileDownloader implements Runnable {
     wc.getOptions().setCssEnabled(false);
     // For access to critical elements on the netzonline website, javascript must be enabled
     wc.getOptions().setJavaScriptEnabled(true);
+    wc.getOptions().setThrowExceptionOnScriptError(false);
     return wc;
   }
   
@@ -95,8 +124,9 @@ public class FileDownloader implements Runnable {
    * Finds all headers which reveal meter details. It clicks them all, which reveals the
    * 'Verbräuche' button needed to download the excel file.
    * @param page The meter overview page
+   * @return The number of details on this page
    */
-  private void openAllDetails(HtmlPage page) {
+  private int openAllDetails(HtmlPage page) {
     List<HtmlElement> arrows = page.getByXPath("//header[@data-eservice-fn='account-toggle']");
     for(HtmlElement element : arrows) {
       try {
@@ -105,6 +135,7 @@ public class FileDownloader implements Runnable {
         e.printStackTrace();
       }
     }
+    return arrows.size();
   }
   
   /**
@@ -113,5 +144,56 @@ public class FileDownloader implements Runnable {
    */
   private boolean moreMetersExist(HtmlPage page) {
     return page.getAnchorByText("Weiter »") != null;
+  }
+  
+  /**
+   * Returns a collection of all 'Verbräuche' buttons
+   */
+  private List<HtmlElement> getConsumptionButtons(HtmlPage page) {
+    return page.getByXPath("//footer/nav/ul/li/form/input[@id='overview_~submit']");
+  }
+  
+  /**
+   * Clicks the download button on the details page and returns the WebResponse as an InputStream
+   * @param detailPage The details page
+   * @return The excel file (as InputStream) or null if there is no content to be downloaded
+   * @throws IOException
+   */
+  private InputStream downloadDetailExcelFile(HtmlPage detailPage) throws IOException {
+    HtmlElement button = detailPage.getFirstByXPath("//section[@class='download']/button");
+    if(button == null) {
+      return null;
+    }
+    Page resultPage = button.click();
+    if(resultPage != null) {
+      return resultPage.getWebResponse().getContentAsStream();
+    }
+    return null;
+  }
+
+  /**
+   * Periodically checks if all 'Verbräuche' buttons have been loaded.
+   * @param meterPage The overview page
+   * @param detailsCount Total number of 'Verbräuche' buttons. As long as the detected amount of buttons is not equal to
+   * this amount, the while loop will continue
+   * @return A collection of 'Verbräuche' buttons after they have all loaded.
+   * @throws InterruptedException
+   */
+  private List<HtmlElement> waitForDetailsToLoad(HtmlPage meterPage, int detailsCount)
+      throws InterruptedException {
+    List<HtmlElement> consumptionButtons = getConsumptionButtons(meterPage);
+    
+    // While there are still details to be loaded, wait one second and try again
+    while(consumptionButtons.size() < detailsCount) {
+      Thread.sleep(1000);
+      consumptionButtons = getConsumptionButtons(meterPage);
+//        System.out.println("Waiting..");
+    }
+    return consumptionButtons;
+  }
+  
+  private void clickBackButton(HtmlPage page) throws IOException {
+    HtmlAnchor back = page.getFirstByXPath("//div[@class='breadcrumbs']/ul/li/a");
+    back.click();
   }
 }
