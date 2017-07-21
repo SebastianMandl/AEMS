5950af89e1840569d4f137a47a82c96c6310c0e6
Niklas der Graf <grafniklas98@gmail.com>
2017-07-21 19:01:26 +0200

    Update Worksheet

--- Projekt/Reportbot/src/main/java/at/htlgkr/aems/util/Utils.java
+++ Projekt/Reportbot/src/main/java/at/htlgkr/aems/util/Utils.java
@@ -6,6 +6,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Utils {
   public static String removePortFromUrl(URL url) {
@@ -41,5 +47,27 @@ public class Utils {
       }
     }
     
+  }
+  
+public static String decryptAes(String toDecrypt, String aesKey) {
+    
+    try {
+      MessageDigest sha = MessageDigest.getInstance("SHA-1");
+      byte[] decryptKey = sha.digest(aesKey.getBytes());
+      // Trim length so that the decrypeKey array will be suitable to be turned into an AES key
+      decryptKey = Arrays.copyOf(decryptKey, 16);
+      
+      SecretKeySpec key = new SecretKeySpec(decryptKey, "AES");
+      
+      Cipher decrypter = Cipher.getInstance("AES");
+      decrypter.init(Cipher.DECRYPT_MODE, key);
+      byte[] decryptedOutput = decrypter.doFinal(Base64.getDecoder().decode(toDecrypt));
+      return new String(decryptedOutput);
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    return null;
   }
 }
