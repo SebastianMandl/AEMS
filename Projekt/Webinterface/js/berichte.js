295081e8cc8944b493e2c538b2f57cb6c6463f18
Niklas der Graf <grafniklas98@gmail.com>
2017-07-29 20:11:56 +0200

    Some JQuery stuff regarding reports.

--- Projekt/Webinterface/js/berichte.js
+++ Projekt/Webinterface/js/berichte.js
@@ -1,3 +1,20 @@
 $(function() {
+	addClickListenersToReports();
 	$( "#RadioButtons1" ).buttonset(); 
 });
+
+function editReport(reportName) {
+	var modal = $("#newReport-modal");
+	modal.modal('show');
+	$('#newReportName').attr("value", reportName);
+}
+
+function addClickListenersToReports() {
+	var reportList = $("#reportList");
+	for(var i = 0; i < reportList.children().length; i++) {
+		var report = reportList.children().eq(i);
+		report.click(function() {
+			editReport($(this).attr("rname"));
+		});
+	}
+}
