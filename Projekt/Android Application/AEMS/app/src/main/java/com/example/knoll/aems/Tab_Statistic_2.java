2c88a6a60c60e05f4e531b7aa97e83f5466e9c54
Niklas der Graf <grafniklas98@gmail.com>
2017-08-24 15:17:58 +0200

    Re-added android stuff

---
+++ Projekt/Android Application/AEMS/app/src/main/java/com/example/knoll/aems/Tab_Statistic_2.java
@@ -0,0 +1,21 @@
+package com.example.knoll.aems;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by knoll on 15.08.2017.
+ */
+
+public class Tab_Statistic_2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_statistic_2,  container, false);
+    }
+}
