5b4c432fe07a62ee4501d8ccf129ebd72237a97f
Niklas der Graf <grafniklas98@gmail.com>
2017-09-15 18:07:11 +0200

    Added Aems Web Server Project (NetBeans)

---
+++ Projekt/WebServer/AemsWebServer/web/js/berichte.js
@@ -0,0 +1,24 @@
+$(function() {
+	$( "#RadioButtonsVorperiode" ).buttonset(); 
+});
+
+$(function() {
+	$( "#RadioButtonsAutomaticGenerating" ).buttonset(); 
+});
+
+
+function editReport(reportName) {
+	var modal = $("#newReport-modal");
+	modal.modal('show');
+	$('#newReportName').attr("value", reportName);
+}
+
+function addClickListenersToReports() {
+	var reportList = $("#reportList");
+	for(var i = 0; i < reportList.children().length; i++) {
+		var report = reportList.children().eq(i);
+		report.click(function() {
+			editReport($(this).attr("rname"));
+		});
+	}
+}
---
+++ Projekt/WebServer/AemsWebServer/web/js/loginfunctions.js
@@ -0,0 +1,20 @@
+function loginButtonClicked(){
+	
+}
+
+
+
+
+function functionRegister(){
+		//$.notify("Eine Registrierungsanfrage wird an den Administrator gesendet", "info");	
+
+}
+
+
+
+function functionLogout(){
+	$.notify("Sie werden nun abgemeldet", "info");
+}
+
+
+
