fe06d9d47f30c0525f680d511ec810a7c0a711e5
Sebastian Mandl <sebastian-mandl@gmx.net>
2017-10-16 18:42:28 +0200

    updated work report + updated Database directory

--- AEMSWebService/src/main/java/aems/KeyDetails.java
+++ AEMSWebService/src/main/java/aems/KeyDetails.java
@@ -27,16 +27,7 @@ public class KeyDetails {
     }
     
     public static byte[] getKey() {
-        BigInteger x = new BigInteger(new String(key));
-        x.multiply(new BigInteger(String.valueOf(AuthenticationDetails.getUsername().hashCode())));
-        String finalKey = x.toString().substring(0, DiffieHellmanProcedure.KEY_LENGTH);
-        
-        // todo: write database function
-        
-        // call database function to retrieve user password
-        // salt the users password into the key
-        
-        return finalKey.getBytes();
+        return key;
     }
     
 }
--- AEMSWebService/src/main/java/aems/RestInf.java
+++ AEMSWebService/src/main/java/aems/RestInf.java
@@ -1,29 +1,21 @@
 package aems;
 
 import aems.graphql.Query;
-import at.htlgkr.aems.util.crypto.Decrypter;
-import at.htlgkr.aems.util.crypto.KeyUtils;
-import at.htlgkr.aems.util.key.DiffieHellmanProcedure;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.math.BigDecimal;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  *
@@ -35,93 +27,126 @@ public class RestInf extends HttpServlet {
     // furthermore implement update and put
     
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        final StringBuffer BUFFER = new StringBuffer();
-//        Files.readAllLines(Paths.get("C:\\Users\\Sebastian\\hubiC\\AEMS\\AEMSWebService\\src\\main\\java\\aems\\query")).forEach(new Consumer<String>() {
-//            @Override
-//            public void accept(String s) {
-//                BUFFER.append(s).append("\n");
-//            }
-//        });
-//        String query = BUFFER.toString();//checkRequest(req, resp);
-//        try {
-//            Class.forName("org.postgresql.Driver");
-//        } catch (ClassNotFoundException ex) {
-//            Logger.getLogger(RestInf.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-        
-        HTTPRequest request = checkRequest(req, resp);
-        
-        if(request.equals(HTTPRequest.KEY_EXCHANGE.derive(request.getAuthentication()))) {
-            // this if condition block is entered if a key exchange was requested from the client
-            // key will be exchanged via the Diffie-Hellman-Procedure
-            
-            KeyExchangeService.start(req.getRemoteAddr());
-        } else {
-            //query is encrypted
-            
-            String rawQuery = "";
-            byte[] key = KeyUtils.salt(new BigDecimal(String.valueOf(KeyDetails.getKey())), "master", "jdh34ztkjnPKL67410tzAdfbnI").toString().substring(0, DiffieHellmanProcedure.KEY_LENGTH).getBytes();
-            try {
-                rawQuery = new String(Decrypter.requestDecryption(key, request.getQuery().getBytes()));
-            } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException ex) {
-                Logger.getLogger(RestInf.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-            GraphQLSchema schema = new GraphQLSchema(Query.getInstance());
-            GraphQL ql = GraphQL.newGraphQL(schema).build();
-            ExecutionResult result = ql.execute(rawQuery);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {        
+        String query = checkRequest(req, resp)[0];
 
-            PrintWriter writer = resp.getWriter();
-            try {
-                writer.write(result.getData().toString());
-            } catch(Exception e) {
-                writer.write("{\"error\":\"no data was returned!\"}");
-            }
-            writer.write("\r\n");
-            writer.flush();
+        GraphQLSchema schema = new GraphQLSchema(Query.getInstance());
+        GraphQL ql = GraphQL.newGraphQL(schema).build();
+        ExecutionResult result = ql.execute(query);
+
+        PrintWriter writer = resp.getWriter();
+        try {
+            writer.write(result.getData().toString());
+        } catch(Exception e) {
+            writer.write("{\"error\":\"no data was returned!\"}");
         }
+        writer.write("\r\n");
+        writer.flush();
         
         resp.setStatus(HttpServletResponse.SC_OK);
     }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String[] results = checkRequest(req, resp);
+        String operation = results[1];
+        
+        final StringBuffer SQL = new StringBuffer();
+        final StringBuffer COLUMN_BUFFER = new StringBuffer();
+        final String OP_INSERT = "INSERT";
+        final String OP_UPDATE = "UPDATE";
+        
+        String whereColumn = null;
+        String whereValue = null;
+        
+        JSONObject root = new JSONObject(results[0]);
+        for(String key : root.keySet()) {
+            Object obj = root.get(key);
+            if(!(obj instanceof JSONArray)) {
+                whereColumn = key;
+                if(obj instanceof String) {
+                    whereValue = "'" + obj.toString() + "'";
+                } else {
+                    whereValue = obj.toString();
+                }
+                continue;
+            }
+            
+            JSONArray table = (JSONArray) obj;
+            switch (operation) {
+                case OP_INSERT:
+                    SQL.append("INSERT INTO ").append("aems.").append(key).append(" (%) VALUES (");
+                    break;
+                case OP_UPDATE:
+                    SQL.append("UPDATE ").append("aems.").append(key).append(" SET ");
+                    break;
+                // fail
+                default:
+                    break;
+            }
+            
+            for(Object entry : table) {
+                if(entry instanceof JSONObject) {
+                    JSONObject entryObj = (JSONObject) entry;
+                    for(String column : entryObj.keySet()) {
+                        if(operation.equals(OP_UPDATE)) {
+                            SQL.append(column).append("=").append(entryObj.get(column) instanceof String ? "'" + entryObj.getString(column).replace("\\s", "") + "'" : entryObj.get(column)).append(",");
+                        } else if(operation.equals(OP_INSERT)) {
+                            COLUMN_BUFFER.append(column).append(",");
+                            SQL.append(entryObj.get(column) instanceof String ? "'" + entryObj.getString(column) + "'" : entryObj.get(column)).append(",");
+                        }
+                    }
+                    
+                    SQL.setLength(SQL.length() - 1);
+                    
+                    if(COLUMN_BUFFER.length() > 0) // column buffer is not empty
+                        COLUMN_BUFFER.setLength(COLUMN_BUFFER.length() - 1);
+                }
+            }        
+        }
+        
+        if(operation.equals(OP_UPDATE)) {
+            if(whereColumn == null || whereValue == null) {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Request could not be fulfilled as a required parameter \"where\" is missing!");
+                return;
+            }
+            SQL.append(" WHERE ").append(whereColumn).append(" = ").append(whereValue.replace("\\s", ""));
+        } else if(operation.equals(OP_INSERT)) {
+            SQL.append(")");
+        }
+                    
+        String sqlQuery = SQL.append(";").toString();
+        
+        if(operation.equals(OP_INSERT)) {
+            sqlQuery = sqlQuery.replace("%", COLUMN_BUFFER.toString());
+        }
+        
+        try {
+            DatabaseConnectionManager.getDatabaseConnection().executeSQL(sqlQuery);
+        } catch (SQLException ex) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "The issued SQL statement caused an error while execution!");
+        }
+    }
     
-    private HTTPRequest checkRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    
+    private String[] checkRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         final int TIMEOUT = 10_000; // 10 seconds
         long lastTime = System.currentTimeMillis();
         
         String query = null;
-        String authentication = null;
-        String type = null;
+        String operation = null;
         
-        while(query == null || authentication == null || type == null) {
+        while(query == null || operation == null) {
             if(System.currentTimeMillis() - lastTime >= TIMEOUT) {
-                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Not all post request parameters could be received completely within time!");
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Not all request parameters could be received completely within time!");
                 return null;
             }
             
-            query = req.getParameter("query"); // query
-            authentication = req.getParameter("authentication"); // user-id
-            type = req.getParameter("type"); // raw or encrypted
+            query = req.getParameter("query");
+            operation = req.getParameter("operation");
         }
         
-        if(type.equals("encrypted") && !KeyDetails.isKeySet()) {
-            resp.sendError(HttpServletResponse.SC_FORBIDDEN, "No key for encryption is present! Aborting request! Unsafe communication!");
-            return null;
-        }
-        
-        try {
-            AuthenticationDetails.setUsername(authentication);
-        } catch (NullPointerException e) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
-            return null;
-        }
-        
-        if(query == null) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "No query was sent in the request!");
-            return null;
-        }
-        
-        return new HTTPRequest(authentication, query, type);
+        return new String[]{ query, operation };
     }
 
 }
