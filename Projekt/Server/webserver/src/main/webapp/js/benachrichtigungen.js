65ab91ef7e215273118c3014a0c4b13826fe9556
Niklas der Graf <grafniklas98@gmail.com>
2017-11-15 16:57:42 +0100

    More API Communication

--- Projekt/Server/webserver/src/main/webapp/js/benachrichtigungen.js
+++ Projekt/Server/webserver/src/main/webapp/js/benachrichtigungen.js
@@ -1,56 +1,80 @@
-          var notifications =0; 
-		  var measuredElectricity = 10;
-		  var maxElectricity = 5;
-		  var measuredGas=15;
-		  var maxGas=4;
-		  var measuredWater;
-		  var maxWater;
-		  var report =1;
+$(document).ready(function() {
+   getNotifications(); 
+});
 
-function functionNotifications(){
-		  if(measuredElectricity>maxElectricity){
-			  $.notify("Ihr Stromverbrauch ist höher als sonst!", "warn");
-			  notifications--;
-		  }
-		  if(measuredGas>maxGas){
-			  $.notify("Ihr Gasverbrauch ist höher als sonst!", "warn");
-			  notifications--;
-		  }
-		  if(measuredWater>maxWater){
-			 $.notify("Ihr Wasserverbrauch ist höher als sonst!", "warn");
-			  notifications--;
-			 }
-		  if(report>0){
-			  $.notify("Ein neuer Bericht steht zum Download bereit!", "info");
-			  notifications--;
-		  }
-	
-		if(notifications<0){
-			$('#notificationCounter').text('0');
-		}
-		else{
-			$('#notificationCounter').text(''+notifications);
-	    	}
-  
-	  }  
-
-
-
-window.onload = function(){
-
-	if(measuredElectricity>maxElectricity){
-			  notifications++;
-		  }
-		  if(measuredGas>maxGas){
-			  notifications++;
-		  }
-		  if(measuredWater>maxWater){
-			 notifications++;
-			 }
-		  if(report>0){
-			 notifications++;
-		  }
-	
-	$('#notificationCounter').text(''+notifications);
-		
+function getNotifications() {
+    var userId = 1;
+    var graphQL = `{
+        notifications(user: ${userId}) {
+            name
+            type
+            meters {
+                id
+            }
+        }
+    }`;
+    
+    $.post(API_URL, {query: graphQL}, displayNotifications);
 }
+
+function displayNotifications(data) {
+    for(var noti of data) {
+        // Display
+    }
+}
+
+var notifications = 0;
+var measuredElectricity = 10;
+var maxElectricity = 5;
+var measuredGas = 15;
+var maxGas = 4;
+var measuredWater;
+var maxWater;
+var report = 1;
+
+function functionNotifications() {
+    if (measuredElectricity > maxElectricity) {
+        $.notify("Ihr Stromverbrauch ist höher als sonst!", "warn");
+        notifications--;
+    }
+    if (measuredGas > maxGas) {
+        $.notify("Ihr Gasverbrauch ist höher als sonst!", "warn");
+        notifications--;
+    }
+    if (measuredWater > maxWater) {
+        $.notify("Ihr Wasserverbrauch ist höher als sonst!", "warn");
+        notifications--;
+    }
+    if (report > 0) {
+        $.notify("Ein neuer Bericht steht zum Download bereit!", "info");
+        notifications--;
+    }
+
+    if (notifications < 0) {
+        $('#notificationCounter').text('0');
+    } else {
+        $('#notificationCounter').text('' + notifications);
+    }
+
+}
+
+
+
+window.onload = function () {
+
+    if (measuredElectricity > maxElectricity) {
+        notifications++;
+    }
+    if (measuredGas > maxGas) {
+        notifications++;
+    }
+    if (measuredWater > maxWater) {
+        notifications++;
+    }
+    if (report > 0) {
+        notifications++;
+    }
+
+    $('#notificationCounter').text('' + notifications);
+
+};
--- Projekt/Server/webserver/src/main/webapp/js/statistics/show-charts.js
+++ Projekt/Server/webserver/src/main/webapp/js/statistics/show-charts.js
@@ -1,102 +1,118 @@
+$(document).ready(function() {
+   showStatistics(); 
+});
+
+function showStatistics() {
+    var userId = 1;
+    var q = ` {
+        statistics(user: ${userId}, show_home: true) {
+            name
+            dateFrom
+            dateTo
+            period
+            values
+        }
+    }`;
+}
 
 var l = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
 var av = [];
 var pv = [];
-var t = [1,2,3,4,5,6,7,8,9,10,11,12];
+var t = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 randomValues();
 showChart("statistic_1", l, av, pv, t);
-showChart("statistic_2", ["KW 08", "KW 09", "KW 10", "KW 11"], [150,120,120, 100], [150,130,130,90], t);
+showChart("statistic_2", ["KW 08", "KW 09", "KW 10", "KW 11"], [150, 120, 120, 100], [150, 130, 130, 90], t);
 showChart("statistic_3", l, av, pv, t);
 
 function showChart(canvasId, labels, actualValues, previousValues, temperature) {
 
-var highestTemperature = Math.max.apply(Math, temperature);
-var lowestTemperature = Math.min.apply(Math, temperature);
+    var highestTemperature = Math.max.apply(Math, temperature);
+    var lowestTemperature = Math.min.apply(Math, temperature);
 
-var highestValueA = Math.max.apply(Math, actualValues);
-var highestValueP = Math.max.apply(Math, previousValues);
+    var highestValueA = Math.max.apply(Math, actualValues);
+    var highestValueP = Math.max.apply(Math, previousValues);
 
-var highestValueToDisplay = Math.max(highestValueA, highestValueP);
+    var highestValueToDisplay = Math.max(highestValueA, highestValueP);
 
-var ctx = document.getElementById(canvasId).getContext('2d');
-  
-var mixedChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    datasets: [
-		{
-			label: 'Temperatur',
-			data: temperature,
-			yAxisID: "secondary",
-			fill: false,
-			borderColor: "green",
-			type: 'line'
+    var ctx = document.getElementById(canvasId).getContext('2d');
+
+    var mixedChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            datasets: [
+                {
+                    label: 'Temperatur',
+                    data: temperature,
+                    yAxisID: "secondary",
+                    fill: false,
+                    borderColor: "green",
+                    type: 'line'
+                },
+                {
+                    label: 'Aktuell',
+                    data: actualValues,
+                    backgroundColor: "rgba(255, 202, 68, 1)",
+                    yAxisID: "primary"
+                },
+                {
+                    label: "Vorperiode",
+                    data: previousValues,
+                    backgroundColor: "rgba(255, 202, 68, 0.5)",
+                    yAxisID: "primary"
+                }
+            ],
+            labels: labels
         },
-		{
-          label: 'Aktuell',
-          data: actualValues,
-		  backgroundColor: "rgba(255, 202, 68, 1)",
-		  yAxisID: "primary"
-        }, 
-		{
-			label: "Vorperiode",
-			data: previousValues,
-			backgroundColor: "rgba(255, 202, 68, 0.5)",
-			yAxisID: "primary"
-		}
-		],
-    labels: labels
-  },
-	options: {
-    scales: {
-      yAxes: [{
-        id: 'primary',
-        type: 'linear',
-        position: 'left',
-		ticks: {
-          max: highestValueToDisplay,
-          min: 0,
-		  callback: function(value, index, values) {
-                        return value + " kW";
-                    }
+        options: {
+            scales: {
+                yAxes: [{
+                        id: 'primary',
+                        type: 'linear',
+                        position: 'left',
+                        ticks: {
+                            max: highestValueToDisplay,
+                            min: 0,
+                            callback: function (value, index, values) {
+                                return value + " kW";
+                            }
+                        }
+                    }, {
+                        id: 'secondary',
+                        type: 'linear',
+                        position: 'right',
+                        ticks: {
+                            max: highestTemperature + 10,
+                            min: lowestTemperature - 10,
+                            callback: function (value, index, values) {
+                                return value + " Celsius";
+                            }
+                        }
+                    }]
+            }
         }
-      }, {
-        id: 'secondary',
-        type: 'linear',
-        position: 'right',
-        ticks: {
-          max: highestTemperature + 10,
-          min: lowestTemperature  - 10,
-		  callback: function(value, index, values) {
-                        return value + " Celsius";
-                    }
-        }
-      }]
-    }
-  }
-});
+    });
 }
 /* Checks if the array parameter is null or empty. If not, the values will be added to the cart */
 function addValuesIfNeeded(chart, array, label, color, axisId, chartType) {
-	if(chartType == null) {
-		chartType = "bar";
-	}
-	if(array != null && array != undefined && array.length > 0) {
-	var vals = {
-		  label: label,
-          data: array,
-		  backgroundColor: color,
-		  yAxisID: axisId,
-		  type: chartType
-		};
-	chart.data.datasets.push(vals);
-	}
-	return chart;
+    if (chartType == null) {
+        chartType = "bar";
+    }
+    if (array != null && array != undefined && array.length > 0) {
+        var vals = {
+            label: label,
+            data: array,
+            backgroundColor: color,
+            yAxisID: axisId,
+            type: chartType
+        };
+        chart.data.datasets.push(vals);
+    }
+    return chart;
 }
 
 function randomValues() {
-for(var i = 0; i < l.length; i++) {
-		av.push(Math.floor(Math.random() * 200) + 50);
-		pv.push(Math.floor(Math.random() * 200) + 50);
-}	
+    for (var i = 0; i < l.length; i++) {
+        av.push(Math.floor(Math.random() * 200) + 50);
+        pv.push(Math.floor(Math.random() * 200) + 50);
+    }
 }
