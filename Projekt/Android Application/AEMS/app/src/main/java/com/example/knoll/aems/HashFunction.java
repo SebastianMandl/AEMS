fcb327fb9d0a75223f881b9f06cd2154122d084d
Lukas Knoll <knolli398@gmail.com>
2018-01-09 17:31:38 +0100

    NextProject Stand 9-1-18

---
+++ Projekt/Android Application/AEMS/app/src/main/java/com/example/knoll/aems/HashFunction.java
@@ -0,0 +1,24 @@
+package com.example.knoll.aems;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by knoll on 28.12.2017.
+ */
+
+public class HashFunction {
+
+    private static final String ALGORITHM = "SHA-512";
+    private static final int ITERATIONS = 10000;
+    private static final int SALT_SIZE = 32;
+
+    public static void main(String [] args){
+
+        HashFunction hash = new HashFunction();
+
+
+    }
+
+
+
+}
