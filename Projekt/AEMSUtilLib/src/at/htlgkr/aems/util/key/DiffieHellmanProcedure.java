eef60fc091bc7b90fc7add26030d14949edd6341
Sebastian Mandl <sebastian-mandl@gmx.net>
2018-03-17 19:09:11 +0100

    ---

--- Projekt/AEMSUtilLib/src/at/htlgkr/aems/util/key/DiffieHellmanProcedure.java
+++ Projekt/AEMSUtilLib/src/at/htlgkr/aems/util/key/DiffieHellmanProcedure.java
@@ -167,31 +167,8 @@ public class DiffieHellmanProcedure {
 		ServerSocket server = new ServerSocket(9950);
 		Socket socket = server.accept();
 		
-//		if(BLOCKED_INET_ADDRESSES.containsKey(socket.getInetAddress())) {
-//			Long blockedTime = BLOCKED_INET_ADDRESSES.get(socket.getInetAddress());
-//			if(System.currentTimeMillis() - blockedTime >= (1_000 * 60)) {
-//				BLOCKED_INET_ADDRESSES.remove(socket.getInetAddress());
-//				ACCESSES_PER_ADDRESS_PER_MINUTE.put(socket.getInetAddress(), 0);
-//			} else {
-//				server.close();
-//				socket.close();
-//				return null;
-//			}
-//		}
-//		
-//		if(ACCESSES_PER_ADDRESS_PER_MINUTE.containsKey(socket.getInetAddress())) {
-//			int accessCount = ACCESSES_PER_ADDRESS_PER_MINUTE.get(socket.getInetAddress());
-//			if(accessCount + 1 > TOTAL_ACCESSES_PER_ADDRESS_PER_MINUTE) {
-//				BLOCKED_INET_ADDRESSES.put(socket.getInetAddress(), System.currentTimeMillis());
-//				socket.close();
-//				server.close();
-//				return null;
-//			}
-//		} else {
-//			ACCESSES_PER_ADDRESS_PER_MINUTE.put(socket.getInetAddress(), 1);
-//		}
-		
-		try(BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			
 			final Random RANDOM = new Random();
 			
@@ -208,14 +185,16 @@ public class DiffieHellmanProcedure {
 			// send key confirmation request
 			//Socket clientSocket = new Socket(socket.getInetAddress().toString(), socket.getPort() + 1);
 			// don't attempt to connect to the client side since port forwarding can not be configured at the client side!!!
-			try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+			try {
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 				writer.write("{combination:" + myCombination.toString() + "}");
 				writer.write("\r\n");
+				writer.flush();
 			} catch(IOException e) {
 				e.printStackTrace();
-			} finally {
-				socket.close();
 			}
+			
+			server.close();
 			
 			BigDecimal key = compute(combination, modNumber, secretNumber);
 			return key.toString().substring(0, KEY_LENGTH).getBytes();
