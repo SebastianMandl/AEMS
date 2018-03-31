d92b8439ea92cd845b152182cb53d261e24e28e9
Sebastian Mandl <sebastian-mandl@gmx.net>
2018-03-31 13:55:42 +0200

    ---

--- Projekt/AEMSElectricityMeterPlugIn/src/AEMSElecricityPlugIn.java
+++ Projekt/AEMSElectricityMeterPlugIn/src/AEMSElecricityPlugIn.java
@@ -10,19 +10,24 @@ import at.htlgkr.aems.plugins.PlugIn;
 import at.htlgkr.aems.raspberry.upload.AEMSUploader;
 import at.htlgkr.aems.raspberry.upload.TableData;
 import at.htlgkr.aems.raspberry.upload.UploadPackage;
+import at.htlgkr.aems.settings.MeterTypes;
 import at.htlgkr.aems.settings.ScriptFile;
 import at.htlgkr.aems.settings.Setting;
 
 public class AEMSElecricityPlugIn extends PlugIn {
 
 	public AEMSElecricityPlugIn() {
-		super("Sensor PLUGIN", Setting.getSetting("kwH", new ScriptFile("python", "run.py")));
-		super.getSetting().setMillisUntilRepetition(1000);
+		super("EMeter PLUGIN", Setting.getSetting(MeterTypes.ELECTRICITY, new ScriptFile("python", "run.py")));
+		super.getSetting().setMillisUntilRepetition(1000 * 60 * 15);
 		super.setUploader(new AEMSUploader(this));
 	}
 	
-	final Pattern NUMBER = Pattern.compile("(?<number>\\d+)");
+	//final Pattern NUMBER = Pattern.compile("(?<number>\\d+)");
+	final Pattern POSITION = Pattern.compile(".*1\\.8\\.1\\((?<value>\\d+\\.\\d+)\\*(?<unit>\\w+)\\).*");
 	final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+	// 1.8.1(2107.424*kWh)(00-00-00 00:00)
+
 	
 	@Override
 	public boolean readCyclic(PlugIn plugin, InputStream inputStream) {
@@ -30,13 +35,25 @@ public class AEMSElecricityPlugIn extends PlugIn {
 		
 		try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
 			for(String line = reader.readLine(); line != null; line = reader.readLine()) {
-				Matcher matcher = NUMBER.matcher(line);
+				System.out.println(line);
+				System.out.flush();
+				Matcher matcher = POSITION.matcher(line);
 				if(matcher.find()) {
-					String value = matcher.group("number");
-					System.out.println("--------- read : " + value);
+					String value = matcher.group("value");
+					String unit = matcher.group("unit");
 					super.getUploader().upload(new UploadPackage().addData(new TableData("meter_data")
-							.addData("meter", plugin.getSetting().getMeterId()).addData("measured_value", value).addData("timestamp", FORMAT.format(new Date())).addData("unit", getSetting().getSensorUnit())));
+							.addData("meter", plugin.getSetting().getMeterId()).addData("measured_value", value).addData("timestamp", FORMAT.format(new Date())).addData("unit", unit)));
+				
+					reader.close();
+					return true;
 				}
+//				Matcher matcher = NUMBER.matcher(line);
+//				if(matcher.find()) {
+//					String value = matcher.group("number");
+//					System.out.println("--------- read : " + value);
+//					super.getUploader().upload(new UploadPackage().addData(new TableData("meter_data")
+//							.addData("meter", plugin.getSetting().getMeterId()).addData("measured_value", value).addData("timestamp", FORMAT.format(new Date())).addData("unit", getSetting().getSensorUnit())));
+//				}
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
