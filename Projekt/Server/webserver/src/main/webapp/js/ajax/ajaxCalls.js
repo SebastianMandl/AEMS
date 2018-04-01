ce265bec2ccfc3bb96ba43b40fc828e77d49ba94
Niklas Graf <grafniklas98@gmail.com>
2018-04-01 13:27:36 +0200

    Removed unnecessary files from github repo

--- Projekt/Server/webserver/src/main/webapp/js/ajax/ajaxCalls.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const BASE_AEMS_URL = "http://localhost:8080/webserver/";
-
-function getUserMeters(userId, callback) {
-    let userData = {"userId": userId};
-    $.post(BASE_AEMS_URL + "dummy/meters.json", userData, callback, "json");
-}
-
